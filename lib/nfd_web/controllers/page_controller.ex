b29cd278fff469d75decdbad98eddcd73894b894
dottjt <julius.reade@gmail.com>
2019-02-17 11:05:42 +1100

    hub registration setup gettin there

--- lib/nfd_web/controllers/page_controller.ex
+++ lib/nfd_web/controllers/page_controller.ex
@@ -1,11 +1,6 @@
 defmodule NfdWeb.PageController do
   use NfdWeb, :controller
 
-  def index(conn, _params) do
-    render(conn, "hub.html")
-      # |> redirect(to: Routes.page_path(conn, :hub))
-  end
-
   def hub(conn, _params) do
     render(conn, "hub.html")
   end
