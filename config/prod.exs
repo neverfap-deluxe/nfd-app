2b940ba26f3f40d74d686ae63dfa04e53f6978f6
dottjt <julius.reade@gmail.com>
2019-03-03 09:00:13 +1100

    yikes

--- config/prod.exs
+++ config/prod.exs
@@ -10,12 +10,20 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :nfd, NfdWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT")], # || 4000
-  url: [host: System.get_env("HOST"), port: System.get_env("PORT")], # 80
+  http: [port: System.get_env("PORT")], # :inet6, || 4000
+  url: [host: System.get_env("HOST"), port: System.get_env("PORT")], # port: 80
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
+  root: ".",
   code_reloader: false
+  # https: [
+  #   port: 443,
+  #   otp_app: :nfd,
+  #   keyfile: System.get_env("SSL_KEY_FILE"),
+  #   certfile: System.get_env("SSL_CERT_FILE"),
+  # ]
+
 
 
 # Configure your database
