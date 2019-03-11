289e50faa40790c0dbc430f16b8ebc7f6035f5a0
dottjt <julius.reade@gmail.com>
2019-03-11 16:19:59 +1100

    cake cake.sh3333

--- config/prod.exs
+++ config/prod.exs
@@ -41,7 +41,7 @@ config :nfd, Nfd.SwooshMailer,
 # which you should run after static files are built and
 # before starting your production server.
 config :nfd, NfdWeb.Endpoint,
-  http: [port: 80],
+  http: [port: System.get_env("PORT")],
   url: [host: System.get_env("HOST")], # port: 80
   # https: [
   #   port: 443,
