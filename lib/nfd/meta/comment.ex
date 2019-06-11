8b59ce6d6601aa7b51e85096b52a798abec6eca9
dottjt <julius.reade@gmail.com>
2019-06-11 16:47:14 +1000

    yur

--- lib/nfd/meta/comment.ex
+++ lib/nfd/meta/comment.ex
@@ -1,6 +1,9 @@
 defmodule Nfd.Meta.Comment do
   use Ecto.Schema
   import Ecto.Changeset
+  
+  alias Nfd.Account.User
+  alias Nfd.Meta.Page
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -9,6 +12,11 @@ defmodule Nfd.Meta.Comment do
     field :name, :string
     field :email, :string
     field :message, :string
+    
+    # has_one :parent_message_id, Comment, on_delete: :delete_all
+    # has_one :user_id, User, on_delete: :delete_all
+    # has_one :page_id, Page, on_delete: :delete_all
+
     field :parent_message_id, :string
     field :user_id, :binary_id
     field :page_id, :binary_id
