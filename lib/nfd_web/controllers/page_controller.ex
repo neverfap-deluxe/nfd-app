73c804e753358ddda6dc6eb3c7f7ce3c0c6a1084
Julius Reade <julius.reade@fmclarity.com>
2019-08-19 13:42:37 +1000

    obvs

--- lib/nfd_web/controllers/page_controller.ex
+++ lib/nfd_web/controllers/page_controller.ex
@@ -26,6 +26,7 @@ defmodule NfdWeb.PageController do
   def guides_post_relapse_academy(conn, _params), do: Fetch.fetch_page(conn, NfdWeb.PageView, :guides_post_relapse_academy, "home.html", FetchAccess.fetch_access_array(:guides_post_relapse_academy))
   def guides_complete_understanding(conn, _params), do: Fetch.fetch_page(conn, NfdWeb.PageView, :guides_complete_understanding, "general.html", FetchAccess.fetch_access_array(:guides_complete_understanding))
   def guides_curriculum(conn, _params), do: Fetch.fetch_page(conn, NfdWeb.PageView, :guides_curriculum, "general.html", FetchAccess.fetch_access_array(:guides_curriculum))
+  def guides_fundamental_meditation_series(conn, _params), do: Fetch.fetch_page(conn, NfdWeb.PageView, :guides_fundamental_meditation_series, "general.html", FetchAccess.fetch_access_array(:guides_fundamental_meditation_series))
   # GUIDES END
 
   # PROGRAMS
