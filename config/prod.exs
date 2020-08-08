bb62ef97b84b542ae68e3c3e028460a6404ea201
dottjt <julius_dott@hotmail.com>
2020-08-08 17:22:04 +1000

    backblaze

--- config/prod.exs
+++ config/prod.exs
@@ -22,14 +22,14 @@ config :nfd, Nfd.SwooshMailer,
   access_key: System.get_env("AWS_ACCESS_KEY"),
   secret: System.get_env("AWS_SECRET_KEY")
 
-config :upstream, :upload, timeout: 600_000
+# config :upstream, :upload, timeout: 600_000
 
-config :upstream, :storage,
-  account_id: System.get_env("B2_ACCOUNT_ID"),
-  application_key: System.get_env("B2_APPLICATION_KEY"),
-  bucket_id: System.get_env("B2_BUCKET_ID"),
-  bucket_name: System.get_env("B2_BUCKET_NAME"),
-  service: "b2"
+# config :upstream, :storage,
+#   account_id: System.get_env("B2_ACCOUNT_ID"),
+#   application_key: System.get_env("B2_APPLICATION_KEY"),
+#   bucket_id: System.get_env("B2_BUCKET_ID"),
+#   bucket_name: System.get_env("B2_BUCKET_NAME"),
+#   service: "b2"
 
 config :nfd, :pow_assent,
   providers:
